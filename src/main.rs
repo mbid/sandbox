97259b98e00bd81aada9820ee764685fde49a990
Martin Bidlingmaier <martin.bidlingmaier@mbid.me>
2025-12-15 01:49:06 +0100

    Print stacktrace for errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     if let Err(e) = sandbox::run() {
         eprintln!("Error: {:#}", e);
+        if std::env::var("RUST_BACKTRACE").is_ok() {
+            eprintln!("\nBacktrace:\n{}", e.backtrace());
+        }
         std::process::exit(1);
     }
 }
